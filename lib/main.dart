0fd2d6d8b6b2f6dab9b52dbe8bec40ee2f73414b
diegoveloper <diego.velasquez.lopez@gmail.com>
2020-07-11 19:42:09 -0500

    cities added

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'WeatherFlut',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.light(),
       home: HomePage(),
     );
--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,15 +1,78 @@
 import 'package:flutter/material.dart';
+import 'package:weatherflut/ui/cities/cities_page.dart';
 
 class HomePage extends StatelessWidget {
+  void handleNavigatePress(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) => CitiesPage()
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Stack(
+        fit: StackFit.expand,
         children: [
-          Image.asset(
-            'assets/logo.png',
-            height: 50,
+          Positioned.fill(
+            child: Image.asset(
+              'assets/welcome.jpg',
+            ),
+          ),
+          SafeArea(
+            child: Center(
+              child: Container(
+                constraints: BoxConstraints(
+                  maxWidth: 250.0,
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    Image.asset(
+                      'assets/logo.png',
+                      height: 55,
+                    ),
+                    const SizedBox(
+                      height: 80,
+                    ),
+                    Text(
+                      'Hola,\nBienvenido',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontSize: 34,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      'Qué te parece si agregamos\nuna nueva ciudad?',
+                      textAlign: TextAlign.start,
+                    ),
+                    const SizedBox(
+                      height: 50,
+                    ),
+                    RaisedButton(
+                      color: Colors.white,
+                      elevation: 0,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(
+                          15.0,
+                        ),
+                      ),
+                      child: Text('Agregar ciudad'),
+                      onPressed: () => handleNavigatePress(context),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ],
       ),
